d90f5ffdb3123ff6c51b790c75dc30d5808b0907
WinFlix <moiseszambrano488@gmail.com>
2022-03-29 21:14:14 -0400

    main

--- src/page/PdfView.jsx
+++ src/page/PdfView.jsx
@@ -1,11 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import pdf from '../assets/moiseszambrano.pdf'
 
 const PdfView = () => {
+
+  useEffect(() => {
+    if(typeof window.orientation !== 'undefined'){
+      document.getElementById('downloadPdf').click()
+      window.close()
+    }
+  }, [])
+  
+
   return (
     <>
       <div style={{width: '100%', height: '100%', position: 'absolute'}}>
-       <object data={pdf} type="application/pdf" width='100%' height='100%'></object>
+       <object data={pdf} type="application/pdf" width='100%' height='100%'>
+         <a href={pdf} download='MoisesZambrano-Cv.pdf' id='downloadPdf'>Descargar Pdf</a>
+       </object>
       </div>
     </>
   );
